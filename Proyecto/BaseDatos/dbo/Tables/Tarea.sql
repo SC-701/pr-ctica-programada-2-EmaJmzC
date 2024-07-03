bc58dfaa60e1f5cced23d90baa2e52720ca9ae22
Emanuel Jimenez Calvo <ejmz141@gmail.com>
2024-07-03 10:55:44 -0600

    Estructura Abstracciones y Base de Datos

---
+++ Proyecto/BaseDatos/dbo/Tables/Tarea.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tarea]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Nombre] VARCHAR(50) NULL,
+    [Descripcion] VARCHAR(50) NULL, 
+    [FechaInicio] DATE NULL, 
+    [FechaFinal] DATE NULL, 
+    [Asignado] UNIQUEIDENTIFIER NULL, 
+    [Estado] UNIQUEIDENTIFIER NULL, 
+    [Creador] UNIQUEIDENTIFIER NULL
+)
